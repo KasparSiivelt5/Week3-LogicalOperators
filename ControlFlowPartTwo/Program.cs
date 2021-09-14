34f9f4152a71d51dbdea107bd51e110e42bd1cbf
Opilane TTHK <opilane@tthk.ee>
2021-09-14 10:47:13 +0300

    add switch

---
+++ ControlFlowPartTwo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ControlFlowPartTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Programm küsib kasutajal sisestada PIN-koodi
+            //programm võrdleb sisestatud PIN-koodi arvuga 1234
+            //kui sisestatud PIN-kood on 1234
+            //programm kuvas konsoolis "Tere tulemast"
+            //kui sisestatud PIN-kood on vale ,programm kuvab konsoolis
+            //"Vale PIN. Proovi uuesti"
+
+            {
+                Console.WriteLine("Palun, sisesta PIN:");
+                int userPIN = Convert.ToInt32(Console.ReadLine());
+
+                if (userPIN == 1234)
+                {
+                    Console.WriteLine("Vale PIN. Proovi uuesti.");
+                }
+                else
+                {
+                    Console.WriteLine("Tere tulemast");
+                }
+            }
+        }
+    }
+}
---
+++ SwitchGrading/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SwitchGrading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta oma hinne");
+            int userGrade = Convert.ToInt32(Console.ReadLine());
+
+            switch (userGrade)
+            {
+
+            }
+            
+
+        }
+    }
+}
